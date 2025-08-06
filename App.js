dfdedb083a7cf6133087e568e43170d6344dd5cf
Vini-cods <vinisanto141@gmail.com>
2025-08-06 11:15:19 -0300

    chore: add firebase connection setup and import in App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { bancoExterno } from './firebaseConnection';
 
 export default function App() {
   return (
